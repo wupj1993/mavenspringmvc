0d00bebd5dde405d2a2f1c06cb204d00efa8290a
Server <757671834@qq.com>
2015-07-08 19:17:24 +0800

    在core上加上少些测试的代码，保证测试代码可以运行，并在resource下添加spring自动扫描的配置文件保证在测试的时候不会出现空指针或者说保证@resource的注解可以扫描的到，并加入ioc容器

---
+++ cores/src/test/java/com/wpj/work/CommonTest.java
@@ -0,0 +1,28 @@
+package com.wpj.work;
+
+import com.wpj.service.serviceImpl.CommentServiceImp;
+import com.wpj.work.entity.T_comment;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+/**
+ * Created by Server on 2015/7/6.
+ */
+public class CommonTest extends TestFather {
+    @Resource
+    private CommentServiceImp commentServiceImp;
+    @Test
+    public void testOne(){
+        System.out.println("---------------------------------------------->"+commentServiceImp);
+      commentServiceImp.selectByPrimaryKey(1);
+       commentServiceImp.deleteByPrimaryKey(1);
+    }
+    @Test
+    public void insert(){
+        commentServiceImp.insertSelective(new T_comment("11",1,new Date(),1));
+
+
+    }
+}
